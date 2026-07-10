f17f5f39da1cf453818a98b08bedcdab009bc238
lmeline <loise13@hotmail.fr>
2026-07-10 16:02:44 +0200

    custom font mui

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { SpaContainer } from "pankosmia-rcl";
+import { SpaContainer,fallbackTheme } from "pankosmia-rcl";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import NewAudioTranslationContent from "./pages/NewAudioTranslationContent";
-// import NewBibleContent from "./pages/NewTextTranslationContent";
-// import NewTextTranslationBook from "./pages/NewTextTranslationBook";
-// import UsfmExport from "./pages/Export/UsfmExport";
-// import PdfGenerate from "./pages/Export/PdfGenerate";
-// import UsfmImport from "./pages/Import/UsfmImport";
 import App from "./App";
-// import DeleteTextTranslationBook from "./pages/DeleteTextTranslationBook";
 import { ThemeProvider } from "@emotion/react";
 import { useEffect, useState } from "react";
 import { getAndSetJson } from "pankosmia-lib/http";
@@ -25,34 +19,9 @@ const router = createHashRouter([
     path: "/createDocument/audioTranslation",
     element: <NewAudioTranslationContent />,
   },
-  // {
-  //   path: "deleteBook",
-  //   element: <DeleteTextTranslationBook />,
-  // },
-  // {
-  //   path: "importBook",
-  //   element: <UsfmImport />,
-  // },
-  // {
-  //   path: "/export/usfm",
-  //   element: <UsfmExport />,
-  // },
-  // {
-  //   path: "/export/pdf",
-  //   element: <PdfGenerate />,
-  // },
 ]);
 function AppLayout() {
-  const [themeSpec, setThemeSpec] = useState({
-    palette: {
-      primary: {
-        main: "#666",
-      },
-      secondary: {
-        main: "#888",
-      },
-    },
-  });
+  const [themeSpec, setThemeSpec] = useState(fallbackTheme);
 
   useEffect(() => {
     if (
